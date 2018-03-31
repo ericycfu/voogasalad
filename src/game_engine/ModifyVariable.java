4d188c8f885ba99160bb890268c3eaeebf4fab7b
RAYAN TOFIQUE <rt129@duke.edu>
2018-03-31 17:00:59 -0400

    Minor refactoring

--- src/game_engine/ModifyVariable.java
+++ src/game_engine/ModifyVariable.java
@@ -13,13 +13,15 @@ public class ModifyVariable implements CustomFunction {
 	private double delta;
 	private double rate;
 	
-	public ModifyVariable(String variable, double delta, double rate)
+	public ModifyVariable()
+	{}
+	
+	public void setParameters(String variable, double delta, double rate)
 	{
 		this.variable = variable;
 		this.delta = delta;
 		this.rate = rate;
 	}
-	
 	
 	/**
 	 * Will get variable list from object and subtract from relevant variable
--- src/game_object/PropertyNotFoundException.java
+++ src/game_object/PropertyNotFoundException.java
@@ -3,7 +3,7 @@ package game_object;
 public class PropertyNotFoundException extends Exception {
 
 	/**
-	 * 
+	 * Exception if user a attempts to interact with a unity property that is non existent
 	 */
 	private static final long serialVersionUID = 1L;
 
