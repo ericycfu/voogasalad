a54612461b8a33a79c54c6b7882ab2d453e2a5a4
Aditya Sridhar <as734@duke.edu>
2018-04-02 19:06:00 -0400

    Began creating design tab label components

--- src/gui_elements/texts/StartScreenText.java
+++ src/gui_elements/texts/StartScreenText.java
@@ -4,7 +4,7 @@ import javafx.scene.text.Text;
 
 public class StartScreenText extends Text {
 	public StartScreenText() {
-		this.setText("RapTileSwagger");
+		this.setText("RapTiltSwagger");
 //		this.setStyle( "-fx-font: 100px Tahoma,"
 //				+ "-fx-fill: linear-gradient(from 0% 0% to 100% 200%, repeat, aqua 0%, red 50%)"
 //				+ "-fx-stroke: black"
