ad90b14ddf82d1d825860878c501d0fc3a6fc628
xy72 <xy72@duke.edu>
2018-04-03 16:28:19 -0400

    Renamed CreatedObjectTab

--- src/authoring/CreatedObjectsTabs.java
+++ src/authoring/CreatedObjectsTabs.java
@@ -4,11 +4,11 @@ import game_view.tabs.ObjectTypeTab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TabPane.TabClosingPolicy;
 
-public class CreatedObjects extends TabPane {
-	public CreatedObjects() {
+public class CreatedObjectsTabs extends TabPane {
+	public CreatedObjectsTabs() {
 		this.getTabs().addAll(
-				new ObjectTypeTab("Building"), 
-				new ObjectTypeTab("Unit"));
+				new ObjectTypeTab("Buildings"), 
+				new ObjectTypeTab("Units"));
 		this.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
 
 	}
