ca3318d03e46f4a88a8b4fd286bccaba9819954a
Frank <yy145@duke.edu>
2018-04-05 15:40:49 -0400

    add VisualUpdate interface

--- src/game_player/visual_element/MiniMap.java
+++ src/game_player/visual_element/MiniMap.java
@@ -1,5 +1,13 @@
 package game_player.visual_element;
 
+import java.util.List;
+
+import game_object.GameObject;
+import game_player.Element;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
 /**
  * Any unit that exists on the explored terrain would be shown in the MiniMap.
  * 
@@ -8,7 +16,62 @@ package game_player.visual_element;
  * @author FY
  *
  */
-public class MiniMap implements Element {
+public class MiniMap implements Element, VisualUpdate {
+	private ImageView myMiniMapDisplay;
 	
+	public MiniMap(double xcoor, double ycoor, double length, double width, ImageView thisMiniMapDisplay) {
+		
+	}
+	
+	@Override
+	public void update(List<GameObject> allGameObjects) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private 
+	/**
+	 * returns the current mini-map to 
+	 * @return
+	 */
+	public ImageView getCurrentMiniMapDisplay() {
+		return new ImageView();
+	}
+	
+	@Override
+	public void setX(double x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setY(double y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setWidth(double w) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setHeight(double h) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Pane getPane() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addNode(Node n) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
