c2e2fd1f7ea330901cf73b41ec054c51901289a2
Siyuan Chen <sc456@duke.edu>
2018-04-05 23:59:56 -0400

    unit action display almost done

--- src/game_player/visual_element/UnitActionDisplay.java
+++ src/game_player/visual_element/UnitActionDisplay.java
@@ -1,28 +1,27 @@
 package game_player.visual_element;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import game_object.GameObject;
-import game_player.Element;
-import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 
-public class UnitActionDisplay implements Element, VisualUpdate{
+public class UnitActionDisplay implements VisualUpdate{
 	
 	public static final int ACTION_GRID_LENGTH = 4;
 	public static final int ACTION_GRID_WIDTH = 3;
 	public static final int CELL_SIZE = 50;
 	private double myLeftCornerXCoor;
 	private double myLeftCornerYCoor;
-	ImageView[][] myActionsGrid;
+	private String myCurrentAction;
+	SkillButton[][] myActionsGrid;
 	Map<String, List<String>> myUnitSkills;
-	Map<String, List<Image>> mySkillImages;
+	Map<String, Image> mySkillImages;
 	
 	public UnitActionDisplay(double xcoor, double ycoor, Map<String, List<String>> unitSkills, 
-			Map<String, List<Image>> skillImages) {
+			Map<String, Image> skillImages) {
 		mySkillImages = skillImages;
 		myUnitSkills = unitSkills;
 		myLeftCornerXCoor = xcoor;
@@ -31,11 +30,13 @@ public class UnitActionDisplay implements Element, VisualUpdate{
 	}
 	
 	private void initialize() {
-		myActionsGrid = new ImageView[ACTION_GRID_LENGTH][ACTION_GRID_WIDTH];
+		myCurrentAction = "";
+		myActionsGrid = new SkillButton[ACTION_GRID_LENGTH][ACTION_GRID_WIDTH];
 		for (int i = 0; i < myActionsGrid.length; i++) {
 			for (int j = 0; j < myActionsGrid[0].length; j++) {
 				Image img = new Image("images/default_icon.png");
-				ImageView cell = new ImageView(img);
+				SkillButton cell = new SkillButton();
+				cell.setGraphic(new ImageView(img));
 				cell.setLayoutX(myLeftCornerXCoor + 50*i);
 				cell.setLayoutY(myLeftCornerYCoor + 50*j);
 				myActionsGrid[i][j] = cell;
@@ -45,45 +46,25 @@ public class UnitActionDisplay implements Element, VisualUpdate{
 
 	@Override
 	public void update(List<GameObject> gameObjects) {
+		if (gameObjects.isEmpty()) return;
 		GameObject gameObject = gameObjects.get(0);
-		
-		
+		ArrayList<String> skills = (ArrayList<String>) myUnitSkills.get(gameObject.getName());
+		for (int i = 0; i < skills.size(); i++) {
+			if (i < 12) {
+				SkillButton curr = myActionsGrid[i%4][i/4];
+				curr.setSkillName(skills.get(i));
+				curr.setGraphic(new ImageView(mySkillImages.get(skills.get(i))));
+				curr.setOnAction(e -> {
+					myCurrentAction = curr.getSkillName();
+					//TO-BE-COMPLETED
+				});
+			}
+			else return;
+		}
 	}
-
-	@Override
-	public void setX(double x) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setY(double y) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setWidth(double w) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setHeight(double h) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Pane getPane() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addNode(Node n) {
-		// TODO Auto-generated method stub
-		
+	
+	public String getCurrentAction() {
+		return myCurrentAction;
 	}
 	
 }
