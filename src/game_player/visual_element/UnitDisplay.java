210efb12a6725de9b9b8a80934b50182c17cbc01
Frank <yy145@duke.edu>
2018-04-05 23:35:01 -0400

    edited

--- src/game_player/visual_element/UnitDisplay.java
+++ src/game_player/visual_element/UnitDisplay.java
@@ -1,7 +1,12 @@
 package game_player.visual_element;
 
+import java.util.List;
+import java.util.Map;
+
+import game_object.GameObject;
 import game_player.Element;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
 /**
@@ -9,19 +14,41 @@ import javafx.scene.layout.Pane;
  * @author Siyuan Chen
  *
  */
-public class UnitDisplay implements Element {
+public class UnitDisplay implements Element, VisualUpdate {
+	private UnitInfoDisplay myInfoDisp;
+	private UnitActionDisplay myActionDisp;
+	private Map<String, List<String>> myUnitSkillsMap;
+	private Map<String, Image> mySkillImagesMap;
+	private double myInfoDispXcoor;
+	private double myInfoDispYcoor;
+	private double myActionDispXcoor;
+	private double myActionDispYcoor;
 	
-	UnitActionDisplay myActionDisp;
-	UnitInfoDisplay myInfoDisp;
-	
-	public UnitDisplay() {
-		
+	public UnitDisplay(double infoDispXcoor, double infoDispYcoor, double actionDispXcoor, double actionDispYcoor, Map<String, List<String>> unitSkills, Map<String, Image> skillImages) {
+		initializeUnitDisplayVariables(infoDispXcoor, infoDispYcoor, actionDispXcoor, actionDispYcoor, unitSkills, skillImages);
+		initializeUnitDisplayComponents();
 	}
 	
-	private void initialize() {
-		
+	private void initializeUnitDisplayVariables(double infoDispXcoor, double infoDispYcoor, double actionDispXcoor, double actionDispYcoor, Map<String, List<String>> unitSkills, Map<String, Image> skillImages) {
+		myUnitSkillsMap = unitSkills;
+		mySkillImagesMap = skillImages;
+		myInfoDispXcoor = infoDispXcoor;
+		myInfoDispYcoor = infoDispYcoor;
+		myActionDispXcoor = actionDispXcoor;
+		myActionDispYcoor = actionDispYcoor;
+	}
+	
+	private void initializeUnitDisplayComponents() {
+		myInfoDisp = new UnitInfoDisplay(myInfoDispXcoor, myInfoDispYcoor);
+		myActionDisp = new UnitActionDisplay(myActionDispXcoor, myActionDispYcoor. myUnitSkillsMap, mySkillImagesMap);
 	}
 
+	@Override
+	public void update(List<GameObject> selectedGameObjects) {
+		myInfoDisp.update(selectedGameObjects);
+		myInfoDisp.update(selectedGameObjects);
+	}
+	
 	@Override
 	public void setX(double x) {
 		// TODO Auto-generated method stub
