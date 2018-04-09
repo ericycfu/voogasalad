fbb7b8c1867ce63f6bb43b952ac9ca96f214fd14
Andrew Yeung <ahy7@duke.edu>
2018-04-09 03:05:05 -0400

    Death Function

---
+++ src/interactions/DeathFunction.java
@@ -0,0 +1,27 @@
+package interactions;
+
+import game_object.GameObject;
+
+public class DeathFunction implements CustomFunction{
+	private CustomFunctionParameterFormat format;
+	@Override
+	public void Execute(GameObject current, GameObject other) {
+		other.setIsDead(true);
+		
+	}
+
+	@Override
+	public CustomFunctionParameterFormat getParameterFormat() {
+		// TODO Auto-generated method stub
+		return format;
+	}
+
+	@Override
+	public void setParameterFormatFields() {
+		format = new CustomFunctionParameterFormat();
+		format.addHelpText("This function allows you to instantly destroy a unit");
+		
+	}
+	
+
+}
