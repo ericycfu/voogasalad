58bcfc8f77aad15b9856e51429f123a776f21aad
Jamie <nk143@duke.edu>
2018-04-10 22:56:14 -0400

    added resource feature on game settings

--- src/gui_elements/buttons/AddResourceButton.java
+++ src/gui_elements/buttons/AddResourceButton.java
@@ -1,35 +1,35 @@
 package gui_elements.buttons;
 
 import javafx.geometry.Pos;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 
 public class AddResourceButton extends MainButton {
 
 	private static final String FILENAME = "add_resource_button.properties";
-	private FlowPane resource_name_pane, resource_amount_pane;
-	private static final int CB_SIZE = 10;
-	private static final Pos CB_POSITION = Pos.CENTER;
-
-	public AddResourceButton() {
-//	Pane resource_name_pane, Pane resource_amount_pane ) {
+	private FlowPane attribute_names_pane, attribute_values_pane;
+	private static final int TEXTFIELD_SIZE = 10;
+	private static final Pos TEXTFIELD_POSITION = Pos.CENTER;
+	
+	public AddResourceButton(Pane attribute_names_pane, Pane attribute_values_pane) {
 		super(FILENAME);
-//		this.resource_name_pane = (FlowPane) resource_name_pane;
-//		this.resource_amount_pane = (FlowPane) resource_amount_pane;
+		this.attribute_names_pane = (FlowPane) attribute_names_pane;
+		this.attribute_values_pane = (FlowPane) attribute_values_pane;
 	}
 
 	@Override
 	protected void setAction() {
 		getButton().setOnAction(value -> {
-			createComboBox(resource_name_pane);
-			createComboBox(resource_amount_pane);
+			createTextField(attribute_names_pane);
+			createTextField(attribute_values_pane);
 		});
 	}
 	
-	private void createComboBox(Pane pane){
-		ComboBox resource_field = new ComboBox();
-		resource_field.setPrefSize(pane.getWidth(), pane.getHeight() / CB_SIZE);
-		pane.getChildren().add(resource_field);
+	private void createTextField(Pane pane) {
+		TextField attribute_field = new TextField();
+		attribute_field.setAlignment(TEXTFIELD_POSITION);
+		attribute_field.setPrefSize(pane.getWidth(), pane.getHeight() / TEXTFIELD_SIZE);
+		pane.getChildren().add(attribute_field);
 	}
 }
--- src/gui_elements/tabs/GameSettingsTab.java
+++ src/gui_elements/tabs/GameSettingsTab.java
@@ -1,7 +1,14 @@
 package gui_elements.tabs;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import game_object.ObjectAttributes;
 import gui_elements.buttons.AddResourceButton;
+import gui_elements.buttons.ResourceApplyButton;
 import gui_elements.combo_boxes.ChooseAmountComboBox;
 import gui_elements.combo_boxes.ChooseResourceComboBox;
 import gui_elements.combo_boxes.LossConditionComboBox;
@@ -13,6 +20,9 @@ import gui_elements.labels.GameSettingsLabel;
 import gui_elements.labels.LossConditionLabel;
 import gui_elements.labels.NumPlayersLabel;
 import gui_elements.labels.StartingAmountLabel;
+import gui_elements.panes.MainPane;
+import gui_elements.panes.ResourceAmountPane;
+import gui_elements.panes.ResourceNamePane;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Group;
@@ -20,6 +30,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class GameSettingsTab extends Tab {
 
@@ -27,10 +38,15 @@ public class GameSettingsTab extends Tab {
 	private Label game_settings_label, num_players_label, loss_condition_label, 
 	create_resource_label, choose_resource_label, starting_amount_label;
 	private GridPane setting_pane;
+	private MainPane resource_name_pane, resource_amount_pane;
+	private ObjectAttributes objAttr;
 	private MainComboBox num_players_cb, loss_condition_cb, create_resource_cb,
-				choose_resource_cb, choose_amount_cb;
+	choose_resource_cb, choose_amount_cb;
+	private Stage stage;
 
 	public GameSettingsTab() {
+//		ObjectAttributes objAttr) {
+//		this.objAttr = objAttr;
 		initialize();
 	}
 
@@ -40,24 +56,28 @@ public class GameSettingsTab extends Tab {
 		setPane();
 		setLabel();
 		setComboBoxes();
-		setButtons();
+//		setButtons();
 	}
-	
+
 	private void setText(){
 		this.setText("GAME SETTING");
 	}
-	
+
 	private void setGroup() {
 		design_root = new Group();
 		this.setContent(design_root);
-		
+
 	}
-	
+
 	private void setPane() {
 		setting_pane = new GridPane();
-		design_root.getChildren().add(setting_pane);
+
+		//		resource_name_pane = new ResourceNamePane(objAttr);
+		//		resource_amount_pane = new ResourceAmountPane(objAttr);
+		design_root.getChildren().addAll(setting_pane);
+		//				resource_name_pane.getPane(), resource_amount_pane.getPane());
 	}
-	
+
 	private void setLabel() {
 		game_settings_label = new GameSettingsLabel().getLabel();
 		num_players_label = new NumPlayersLabel().getLabel();
@@ -65,9 +85,9 @@ public class GameSettingsTab extends Tab {
 		create_resource_label = new CreateResourceLabel().getLabel();
 		choose_resource_label = new ChooseResourceLabel().getLabel();
 		starting_amount_label = new StartingAmountLabel().getLabel();
-		
+
 		design_root.getChildren().addAll(game_settings_label, num_players_label,loss_condition_label,
-									create_resource_label, choose_resource_label, starting_amount_label);
+				create_resource_label, choose_resource_label, starting_amount_label);
 	}
 
 	private void setComboBoxes() {
@@ -79,9 +99,9 @@ public class GameSettingsTab extends Tab {
 						);
 		num_players_cb = new NumPlayerComboBox();
 		num_players_cb.getItems().addAll(num_players_options);
-//		num_paleyrs_cb = new ComboBox(num_player_options);
+		//		num_paleyrs_cb = new ComboBox(num_player_options);
 		num_players_cb.setValue("1");
-		
+
 		ObservableList<String> loss_condition_options = 
 				FXCollections.observableArrayList(
 						"1",
@@ -90,8 +110,8 @@ public class GameSettingsTab extends Tab {
 						);
 		loss_condition_cb = new LossConditionComboBox();
 		loss_condition_cb.setValue("3");
-		
-		
+
+
 		choose_resource_cb = new ChooseResourceComboBox();
 		choose_resource_cb.setValue("gold");
 		ObservableList<String> resource_options = 
@@ -101,8 +121,8 @@ public class GameSettingsTab extends Tab {
 						"crystal"
 						);
 		choose_resource_cb.getItems().addAll(resource_options);
-		
-		
+
+
 		choose_amount_cb = new ChooseAmountComboBox();
 		choose_amount_cb.setValue("10");
 		ObservableList<String> amount_options = 
@@ -111,18 +131,28 @@ public class GameSettingsTab extends Tab {
 						"60", "70", "80", "90", "100"
 						);
 		choose_amount_cb.getItems().addAll(amount_options);
-		
+
 		design_root.getChildren().addAll(num_players_cb.getComboBox(), 
 				loss_condition_cb.getComboBox(), choose_resource_cb.getComboBox(),
 				choose_amount_cb.getComboBox());
-		
+
 
 
 	}
-	
+
 	private void setButtons() {
-		design_root.getChildren().addAll(new AddResourceButton().getButton());
+		design_root.getChildren().addAll(new AddResourceButton(resource_name_pane.getPane(), resource_amount_pane.getPane()));
+		//					new ResourceApplyButton(resource_name_pane.getPane(),
+		//	  						 resource_amount_pane.getPane(),
+		//	  						 stage,
+		//	  						objAttr));
+		//		resource_name_pane.getPane(), 
+		//			resource_amount_pane.getPane()).getButton());
+
+
 	}
+
+
 
 
 }
