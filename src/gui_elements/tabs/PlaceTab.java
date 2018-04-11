1334ffebd2ea21cde6de32ca0417b0539e56be6b
xy72 <xy72@duke.edu>
2018-04-11 00:27:21 -0400

    Added heiarchy on everything, working modification of AuthoringObject

--- src/gui_elements/tabs/PlaceTab.java
+++ src/gui_elements/tabs/PlaceTab.java
@@ -1,19 +1,22 @@
 package gui_elements.tabs;
 
 import authoring.backend.AuthoringController;
+import authoring.backend.CreatedMaps;
 import authoring.backend.DraggableScrollPane;
+import authoring.backend.GameEntity;
 import javafx.scene.control.Tab;
 
 public class PlaceTab extends Tab {
 	private DraggableScrollPane myScroll;
-	public PlaceTab(AuthoringController ac) {
+	public PlaceTab(AuthoringController ac, GameEntity game) {
 		this.setText("Place");
-		myScroll = new DraggableScrollPane(ac);
+		myScroll = new DraggableScrollPane(ac, game);
 		this.setContent(myScroll);
 	}
 	
 	public DraggableScrollPane getScroll() {
 		return myScroll;
 	}
+	
 	
 }
