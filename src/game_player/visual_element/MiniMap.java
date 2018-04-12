a7e30e34b231cc362c09c006dabce79cf2025d14
Andrew Yeung <ahy7@duke.edu>
2018-04-12 14:58:29 -0400

    Fixed a print statement

--- src/game_player/visual_element/MiniMap.java
+++ src/game_player/visual_element/MiniMap.java
@@ -47,7 +47,6 @@ public class MiniMap implements VisualUpdate {
 	
 	@Override
 	public void update(List<GameObject> allGameObjects) {
-		System.out.println(myMiniMap.getChildren());
 		myVisibleUnits.getChildren().clear();
 		currentVisibleUnits = filter(allGameObjects);
 		displayUnits(currentVisibleUnits);
