b03de02457010d72d64ef7c7b32a2b931ded9ebb
Andrew Yeung <ahy7@duke.edu>
2018-04-11 23:31:37 -0400

    Fixed compile errors

--- src/authoring/view/Map.java
+++ src/authoring/view/Map.java
@@ -2,9 +2,9 @@ package authoring.view;
 
 public class Map {
 	
-	map_size;
-	background_image;
-	num_players;
+//	map_size;
+//	background_image;
+//	num_players;
 
 	public Map() {
 		// TODO Auto-generated constructor stub
