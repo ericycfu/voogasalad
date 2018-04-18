c3eacc0a037dc6aaf0f0ff02993cf7911c099858
Frank <yy145@duke.edu>
2018-04-17 23:07:20 -0400

    add functionalities to skillbutton class

--- src/game_player/visual_element/SkillButton.java
+++ src/game_player/visual_element/SkillButton.java
@@ -1,16 +1,50 @@
 package game_player.visual_element;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class SkillButton extends Button {
 
 	private String mySkillName;
-
+	private Image mySkillImage;
+	private String mySkillDescription;
+	private int myInteractionID;
+	private ImageView mySkillImageView;
+	
+	public SkillButton() {
+		
+	}
+	
+	public SkillButton(Image skillImage, String interactionName, int interactionNumber, String skillDescription) {
+		mySkillImage = skillImage;
+		mySkillName = interactionName;
+		myInteractionID = interactionNumber;
+		mySkillDescription = skillDescription;
+	}
+	
+	public void setGraphic(double width) {
+		mySkillImageView = new ImageView(mySkillImage);
+		mySkillImageView.setFitHeight(width);
+		mySkillImageView.setFitWidth(width);
+		this.setGraphic(mySkillImageView);
+	}
+	
 	public String getSkillName() {
 		return mySkillName;
+	}
+	
+	public String getSkillDescription() {
+		return mySkillDescription;
 	}
 	
 	public void setSkillName(String skillname) {
 		mySkillName = skillname;
 	}
+	
+	public int getInteractionID() {
+		return myInteractionID;
+	}
+	
 }
+
