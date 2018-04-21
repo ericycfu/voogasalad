bd5cea370e8c86a7e85e16795714e42aa6cb7b8f
Frank <yy145@duke.edu>
2018-04-20 20:37:56 -0400

    unit interaction implemented

--- src/game_player/SelectedUnitManager.java
+++ src/game_player/SelectedUnitManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import game_object.GameObject;
 import game_object.GameObjectManager;
+import interactions.Interaction;
 import transform_library.Vector2;
 
 public class SelectedUnitManager {
@@ -28,9 +29,18 @@ public class SelectedUnitManager {
 	}
 	
 	public void takeInteraction(Vector2 position, GameObject target, int interactionID, GameObjectManager gom) {
+		GameObject top = selectedUnits.get(0);
+		String interactionName = top.accessLogic().accessInteractions().getInteraction(interactionID).getName();
 		for (GameObject go : selectedUnits){
-			go.queueInteraction(position, target, interactionID, gom);
+			boolean isInteractionValid = false;
+			for (Interaction i : go.accessLogic().accessInteractions().getElements()) {
+				isInteractionValid = i.getName().equals(interactionName);
+			}
+			if (isInteractionValid) {
+				go.queueInteraction(position, target, interactionID, gom);
+			}
 		}
+		//TO-DO: build interaction - only the top unit does build 
 	}
 	
 	public List<GameObject> getSelectedUnits(){
