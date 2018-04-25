ce41a9a1b80bd49c1e534a22a10db693ba56c4f0
Shicheng Rao <sr307@duke.edu>
2018-04-24 21:45:39 -0400

    make game

--- src/game_data/AuthoringToGameObject.java
+++ src/game_data/AuthoringToGameObject.java
@@ -1,10 +1,12 @@
 package game_data;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import authoring.backend.AuthoringObject;
 import authoring.backend.DraggableImageView;
+import game_object.GameObject;
 import game_object.GameObjectManager;
 import transform_library.Transform;
 import transform_library.Vector2;
@@ -12,10 +14,10 @@ import transform_library.Vector2;
 public final class AuthoringToGameObject {
 
 	private AuthoringToGameObject() {
-		// TODO Auto-generated constructor stub
+
 	}
 
-	public static GameObjectManager convert(Map<AuthoringObject, List<DraggableImageView>> map) {
+	public static GameObjectManager convertMap(Map<AuthoringObject, List<DraggableImageView>> map) {
 		GameObjectManager GOM = new GameObjectManager();
 		for(AuthoringObject AO: map.keySet()) {
 			for(DraggableImageView DIV: map.get(AO)) {
@@ -24,6 +26,20 @@ public final class AuthoringToGameObject {
 		}
 		
 		return GOM;
+	}
+	/**
+	 * currently doesn't work, need gameObjectConstructor
+	 * @param list
+	 * @return
+	 */
+	public static List<GameObject> convertList(List<AuthoringObject> list){
+		List<GameObject> gameObjectList = new ArrayList<>();
+		for(AuthoringObject AO: list) {
+			//gameObjectList.add(new GameObject())
+		}
+		
+		
+		return gameObjectList;
 	}
 	
 }
