fda28c1911746a9b5202cbfe0137c255425a39b1
Eric Fu <ef105@duke.edu>
2018-04-24 20:53:26 -0400

    added saving functionality to the resources tab

--- src/authoring/view/MapSettingsView.java
+++ src/authoring/view/MapSettingsView.java
@@ -1,11 +1,18 @@
 package authoring.view;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
 import javax.swing.JFileChooser;
 
 import authoring.backend.ButtonFactory;
 import authoring.backend.LabelFactory;
 import authoring.backend.MapSettings;
 import authoring.backend.TextFieldFactory;
+import game_data.Reader;
+import game_data.Writer;
 import game_engine.ResourceManager;
 import gui_elements.buttons.ImageChooserButton;
 import javafx.event.ActionEvent;
@@ -22,12 +29,15 @@ import javafx.scene.layout.VBox;
 
 public class MapSettingsView extends Pane implements AuthoringView {
 	private ResourceManager myResourceManager;
-	
+	private Writer myWriter;
+	private Reader myReader;
 	
 	public MapSettingsView(MapSettings settings) {
 		this.getStyleClass().add(STYLE_PATH);
 		initializeAll();
 		myResourceManager = new ResourceManager();
+		myWriter = new Writer();
+		myReader = new Reader();
 	}
 	
 	private void initializeAll() {
@@ -123,6 +133,28 @@ public class MapSettingsView extends Pane implements AuthoringView {
 		VBox myRootBox = (VBox) this.getChildren().get(0);
 		saveResources((VBox)((HBox) myRootBox.getChildren().get(1)).getChildren().get(0));
 		saveMapConfiguration((VBox)((HBox) myRootBox.getChildren().get(0)).getChildren().get(1));
+		try {
+			myWriter.write("src/gui_elements/tabs/test", myResourceManager.getResourceEntries());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//this try/catch statement below likely goes into the player so they can get the list of resources?
+		try {
+			System.out.println("creating the list");
+			List<Entry<String, Double>> myList = new ArrayList<Entry<String, Double>>();
+			List<Object> initialList = new ArrayList<Object>();
+			initialList = myReader.read("src/gui_elements/tabs/test");
+			for (Object myObj : initialList) {
+				myList.add((Entry<String, Double>) myObj);
+			}
+			System.out.println(myList.get(0));
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private void saveMapConfiguration(VBox myBox) {
