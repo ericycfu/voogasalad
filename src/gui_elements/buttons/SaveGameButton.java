8c4b9c3c060fc33a1535e31b70f2a4ab7f12f837
Aditya Sridhar <as734@duke.edu>
2018-04-26 18:19:52 -0400

    Got images to work, can now modify tags live, added parameter names for custom functions, and made other changes

--- src/gui_elements/buttons/SaveGameButton.java
+++ src/gui_elements/buttons/SaveGameButton.java
@@ -1,13 +1,10 @@
 package gui_elements.buttons;
 
-import game_data.Reader;
 import game_data.Writer;
-import gui_elements.tabs.DesignTab;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 import java.io.IOException;
-import java.util.List;
-
-import authoring.backend.AuthoringObject;
 import authoring.backend.CreatedObjects;
 import resources.Resources;
 
@@ -15,6 +12,8 @@ public class SaveGameButton extends MainButton {
 
 	private static final String FILENAME = "save_game_button.properties";
 	private static final String RESOURCES_STRING = "AUTHOR_LOCATION_OBJECTS";
+	private static final String ALERT_TITLE = "COmponent Saved";
+	private static final String ALERT_MESSAGE = "Your component has been saved!";
 	private static final boolean EXPLICIT_SET_ACTION = false;
 	private Writer myWriter;
 //	private Reader myReader;
@@ -31,12 +30,19 @@ public class SaveGameButton extends MainButton {
 //			myReader = new Reader();
 			try {
 				myWriter.write(Resources.getString(RESOURCES_STRING), CreatedObjects.getAuthoringObjects());
-				System.out.println("Object saved");
+				createAlert();
 			} catch (IOException e) {
 				System.err.println("Could not save created authoring objects");
 			}
 			
 			CreatedObjects.getAuthoringObjects().clear();			
 		});
+	}
+	
+	private void createAlert() {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle(ALERT_TITLE);
+		alert.setContentText(ALERT_MESSAGE);
+		alert.showAndWait();
 	}
 }
