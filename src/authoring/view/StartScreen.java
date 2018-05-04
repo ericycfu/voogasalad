9bd447125304045f6e5b65a4103ea7ab74303236
Eric Fu <ef105@duke.edu>
2018-05-04 02:11:13 -0400

    added error message for load game button

--- src/authoring/view/StartScreen.java
+++ src/authoring/view/StartScreen.java
@@ -7,6 +7,9 @@ import gui_elements.factories.ButtonFactory;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
@@ -47,8 +50,12 @@ public class StartScreen implements AuthoringView {
 															File myFile = myFC.showOpenDialog(new Stage());
 															try {
 																new MakeGameScreen(myStage, myFile);
-															} catch (ClassNotFoundException | IOException e2) {
-																
+															} catch (ClassNotFoundException | IOException | NullPointerException e2) {
+																Alert alert = new Alert(AlertType.ERROR);
+																alert.setTitle("Error Selecting Game");
+																alert.setContentText("You did not choose a file or the file is incorrectly formatted");
+																alert.setHeaderText(null);
+																alert.showAndWait();
 															}
 															
 				}, "image_button"),
